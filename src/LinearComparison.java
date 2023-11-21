b6ae4a29a0abb5e6ab1ed636b63a3d60bb6b6c23
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-21 16:32:36 +0530

    Length Compare

--- src/LinearComparison.java
+++ src/LinearComparison.java
@@ -15,6 +15,13 @@ class Point{
 		double length2 = Math.sqrt( Math.pow((p3.x - p4.x), 2) + Math.pow((p3.y - p4.y), 2));
 		return Double.compare(length1, length2) == 0;
 	}
+	
+	public int compareLengthDifference(Point p2, Point p3, Point p4) {
+	    Double length1 = Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
+	    Double length2 = Math.sqrt(Math.pow((p3.x - p4.x), 2) + Math.pow((p3.y - p4.y), 2));
+	    return length1.compareTo(length2);
+	}
+
 }
 public class LinearComparison {
 
@@ -39,5 +46,13 @@ public class LinearComparison {
 		y = scanner.nextInt();
 		Point p4 = new Point(x, y);
 		System.out.println("Equality check of distance: "+ p1.compareLength(p2, p3, p4));	
+		int compareLength = p1.compareLengthDifference(p2, p3, p4);
+		if (compareLength == 0) {
+		    System.out.println("The lengths are equal.");
+		} else if (compareLength > 0) {
+		    System.out.println("Length1 is greater than Length2.");
+		} else {
+		    System.out.println("Length1 is less than Length2.");
+		}
 	}
 }
